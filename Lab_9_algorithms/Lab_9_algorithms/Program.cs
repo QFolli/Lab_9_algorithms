cd09460f95dd9a82732229721c7477d12007bc6a
Виталий Моисеенко <88559269+QFolli@users.noreply.github.com>
2022-06-13 11:22:59 +0300

    Add files via upload

---
+++ Lab_9_algorithms/Lab_9_algorithms/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_9_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.2. Код
+            Console.WriteLine("Введите два натуральных числа S и K, представляющих сумму и количество цифр соответственно (K <= 100):");
+            string[] temp = Console.ReadLine().Split(' ');
+            int S = Int32.Parse(temp[0]);
+            int K = Int32.Parse(temp[1]);
+            List<int> list = new List<int>();
+            int[] maximum = new int[K];
+            int[] minimum = new int[K];
+            string max = "", min = "";
+            int n, m = 0;
+            int a = 0, b = 0, c = S;
+            for (int i = 9; i > 0; i--)
+            {
+                n = S / i;
+                for (int j = m; j < m + n; j++)
+                {
+                    list.Add(i);
+                }
+                m += n;
+                S = S % i;
+                if (S == 0) break;
+            }
+            S = c;
+            a = 0;
+            b = 0;
+            c = 0;
+            while (true)
+            {
+                b += list[a];
+                if (b <= S)
+                {
+                    maximum[a] = list[a];
+                    c++;
+                    if (b == S) break;
+                }
+                else
+                {
+                    b -= list[a];
+                }
+                a++;
+            }
+            list[list.IndexOf(list.Min())]--;
+            minimum[0] = 1;
+            a = 0;
+            b = 1;
+            c = K - 1;
+            while (true)
+            {
+                b += list[a];
+                if (b <= S)
+                {
+                    minimum[c] = list[a];
+                    if (b == S) break;
+                }
+                a++; c--;
+            }
+            foreach (var item in maximum)
+            {
+                max += item;
+            }
+            foreach (var item in minimum)
+            {
+                min += item;
+            }
+            Console.WriteLine("Наибольшее и наименьшее возможные значения: {0} и {1}", max, min);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab_9_algorithms/Schedule/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Schedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2.3. Расписание
+            Console.Write("Введите количество заявок (N <= 1000) \nN = ");
+            int n = Int32.Parse(Console.ReadLine());
+            Random rand = new();
+            string[] lecture; int s, f;
+            int count = 0, bufer = 0;
+            SortedDictionary<int, List<int>> list = new SortedDictionary<int, List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                lecture = Console.ReadLine().Split(' ');
+                if (list.ContainsKey(Int32.Parse(lecture[0])))
+                {
+                    list[Int32.Parse(lecture[0])].Add(Int32.Parse(lecture[1]));
+                }
+                else
+                {
+                    list[Int32.Parse(lecture[0])] = new List<int>();
+                    list[Int32.Parse(lecture[0])].Add(Int32.Parse(lecture[1]));
+                }
+            }
+            foreach (var item in list)
+            {
+                if (item.Key >= bufer)
+                {
+                    count++;
+                    bufer = item.Value.Min();
+                }
+                else
+                {
+                    if (item.Value.Min() < bufer)
+                    {
+                        bufer = item.Value.Min();
+                    }
+                }
+            }
+            Console.WriteLine("Максимальное количество заявок: ", count);
+            Console.ReadLine();
+        }
+    }
+}
